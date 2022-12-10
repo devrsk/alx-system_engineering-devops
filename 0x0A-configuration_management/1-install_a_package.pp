c11d5e9046709f60f9bb7ac5fac6197ce8afa385
Raheem Salifu Kasim <r.kasim@aol.com>
2022-12-10 00:58:45 +0000

    Task Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installing flask
-package { 'pip install flask':
+#!/usr/bin/pup
+# Install a version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
 }
