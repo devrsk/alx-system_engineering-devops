98b28e6c2dcb85e95fbb7b633561cc060e3307f3
Raheem Salifu Kasim <r.kasim@aol.com>
2022-12-19 22:00:26 +0000

    Task Update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,22 @@
-# automate the task of creating a custom HTTP header response, but with Puppet
+# automate the task of creating a custom HTTP header response
 
-exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
+exec {'sudo chown -R':
+  command => "/usr/bin/sudo /bin/chown -R ${id}:${id} /etc/nginx/sites-available/"
+}
+
+exec {'sudo service nginx':
+  command => '/usr/bin/sudo /usr/sbin/service nginx restart'
+}
+
+file {'/etc/nginx/sites-available/default':
+  ensure => 'file'
+}
+
+-> file_line{'X-Served-By':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  line    => '    server_name _;
+    add_header X-Served-By $hostname;',
+  match   => 'server_name _;',
+  require => Exec['sudo service nginx']
 }
